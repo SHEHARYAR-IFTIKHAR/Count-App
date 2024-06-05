e5427ffc4b0095458a590191727b53f3f9791f2e
Sheharyar Iftikhar <sheharyar.isys@gmail.com>
2024-06-05 12:37:11 -0700

    add javascript and react js hooks

--- CounterApp/src/App.jsx
+++ CounterApp/src/App.jsx
@@ -4,30 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [counter, setCounter] = useState(1);
+  const addValue = () => {
+    setCounter(counter + 1)
+  }
 
+  const extractValue = () => {
+    setCounter(counter - 1)
+  }
+  // let counter = 5;
   return (
+
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='bg-gray-900 h-screen  flex flex-col justify-center items-center'>
+        <h1 className="font-bold text-4xl text-white m-5">
+          SheharyarIftikhar <span className='text-yellow-500'>.</span>
+        </h1>
+
+        <h2 className='text-white mt-3 text-3xl font-bold'>Counter Value: <span className='text-yellow-500'>{counter}</span></h2>
+        <div className="flex gap-3 mt-5">
+          <button className='text-white border px-5 py-3 rounded-md shadow-md font-semibold' onClick={addValue}>Increase Value</button>
+          <button className='text-white border px-5 py-3 rounded-md shadow-md font-semibold' onClick={extractValue}>Decrease Value</button>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
